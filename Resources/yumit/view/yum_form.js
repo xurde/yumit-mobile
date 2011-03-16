d06a2861c2059de11a6cd57e1208964c0ae67948
Pablo Delgado <pablete@gmail.com>
2011-03-16 18:28:42 -0300

    Added upload just works

--- Resources/yumit/view/yum_form.js
+++ Resources/yumit/view/yum_form.js
@@ -4,22 +4,25 @@
   var Dish  = win.Dish;
   var Yumit = win.Yumit;
 
-  var scrollView = Ti.UI.createScrollView({ 
-      backgroundColor:Yumit.styles.darkRed,
-      contentWidth:'auto', 
-      contentHeight:'auto', 
-      top:0, 
-      showVerticalScrollIndicator:true, 
-      showHorizontalScrollIndicator:false, 
+  var scrollView = Ti.UI.createScrollView({
+      backgroundColor:'#eedddd',
+      contentWidth:'auto',
+      contentHeight:'auto',
+      top:0,
+      width:320,
+      height:480,
+      showVerticalScrollIndicator:true,
+      showHorizontalScrollIndicator:false,
       verticalBounce:true
   });
-  
+
   var winview = Ti.UI.createView({
-    top:0, 
+    top:0,
     // backgroundColor:'#FFFFFF',
     contentWidth:320,
     contentHeight:480
   });
+
 
 
   scrollView.add(winview);
@@ -30,92 +33,164 @@
       metaHeight = 14;
 
   var place_name = Ti.UI.createLabel({
-  	color:Yumit.styles.grayTextColor,
-  	font: {
-  		fontFamily:Yumit.styles.fontFamily,
-  		fontSize:14,
-  		fontWeight:'bold'
-  	},
-  	height:'auto',
-  	text:Place.place_name,
-  	width:'auto',
-  	top:10,
-  	right:spacing
+    color:Yumit.styles.textColor,
+    font: {
+      fontFamily:Yumit.styles.fontFamily,
+      fontSize:14,
+      fontWeight:'bold'
+    },
+    height:'auto',
+    text:Place.place_name,
+    width:'auto',
+    top:10,
+    right:spacing
   });
   winview.add(place_name);
-  
+
   var dish_name = Ti.UI.createLabel({
-  	color:Yumit.styles.grayTextColor,
-  	font: {
-  		fontFamily:Yumit.styles.fontFamily,
-  		fontSize:14,
-  		fontWeight:'bold'
-  	},
-  	height:'auto',
-  	text:Dish.dish_name,
-  	width:'auto',
-  	top:25,
-  	right:spacing
+    color:Yumit.styles.textColor,
+    font: {
+      fontFamily:Yumit.styles.fontFamily,
+      fontSize:14,
+      fontWeight:'bold'
+    },
+    height:'auto',
+    text:Dish.dish_name,
+    width:'auto',
+    top:25,
+    right:spacing
   });
   winview.add(dish_name);
-  
+
   var horizontal = function(media){
     return (media.height <  media.width);
   };
-  
+
   var avatar = Ti.UI.createImageView({
-  	top:40,
-  	left:50,
-  	height:horizontal(win.media) ? 80 : 120,
-  	width:horizontal(win.media) ? 120 : 80,
-  	borderRadius:5,
-  	image: win.media
+    top:7,
+    left:7,
+    height:horizontal(win.media) ? 105 : 140,
+    width:horizontal(win.media) ? 140 : 105,
+    borderRadius:5,
+    image: win.media
   });
   winview.add(avatar);
-  
-////////////////////////////////////////////////////////////////////////////////////////
- 
 
+////////////////////////////////////////////////////////////////////////////////////////
 
   var description = Titanium.UI.createTextArea({
-  	value:'Description',
-  	height:100,
-  	width:300,
-  	top:170,
-  	font:{fontFamily:'courier',fontSize:14},
-  	color:'#888',
-  	textAlign:'left',
-  	//appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,	
-  	keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
-  	//returnKeyType:Titanium.UI.RETURNKEY_DONE,
+    value:'Description',
+    height:60,
+    width:300,
+    top:120,
+    font:{fontFamily:'courier',fontSize:14},
+    color:'#888',
+    textAlign:'left',
+    //appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,
+    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
+    returnKeyType:Titanium.UI.RETURNKEY_NEXT,
     borderWidth:2,
     borderColor:'#bbb',
     borderRadius:7,
-  	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
-  	//suppressReturn:false
+    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+    //suppressReturn:false
 
   });
-  winview.add(description);
   description.addEventListener("focus", function(){ description.value='';});
-  description.addEventListener('blur', function() {
-      winview.animate({bottom: 0, duration:500});
+  description.addEventListener('return', function() {
+      tags.focus();
   });
-  
+  winview.add(description);
+
   var tags = Titanium.UI.createTextField({
-    value:'Tags',
-  	color:'#888',
-  	height:35,
-  	top:280,
-  	width:300,
-  	font:{fontFamily:'courier',fontSize:14},
-  	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+    hintText:'Tags',
+    color:'#888',
+    height:35,
+    top:190,
+    width:300,
+    font:{fontFamily:'courier',fontSize:14},
+    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
+    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT
+  });
+  tags.addEventListener('return', function() {
+      scrollView.scrollTo(0,0);
   });
   winview.add(tags);
-  tags.addEventListener("focus", function(){ tags.value='';});
-  tags.addEventListener('blur', function() {
-      winview.animate({bottom: 0, duration:500});
+
+  /////////////////////////////////////////////////////////////////////////////BOTON
+  var ind = Titanium.UI.createProgressBar({
+    width:200,
+    height:50,
+    min:0,
+    max:1,
+    value:0,
+    style:Titanium.UI.iPhone.ProgressBarStyle.BAR,
+    top:240,
+    message:'Upload Progress:',
+    font:{fontSize:12, fontWeight:'bold'},
+    color:Yumit.styles.textColor
   });
- 
+  winview.add(ind);
+//  ind.show();
+
+  var yumit = Titanium.UI.createButton({
+    title:'Yumit!',
+    color:Yumit.styles.darkRed,
+    highlightedColor:'#0f0',
+    width:200,
+    height:40,
+    top:310
+  });
+  winview.add(yumit);
+
+  yumit.addEventListener('click', function (){
+    ind.show();
+    var xhr = Titanium.Network.createHTTPClient();
+    xhr.onerror = function(e) {
+      ind.hide();
+      Titanium.UI.createAlertDialog({
+        title:'Well, this is awkward...',
+        message: 'We had a problem posting your image - please try again'
+      }).show();
+    };
+    xhr.onload = function() {
+      ind.hide();
+      var doc = this.responseXML.documentElement;
+      if (doc.getElementsByTagName("err") != null && doc.getElementsByTagName("err").length > 0) {
+        Titanium.UI.createAlertDialog({
+          title:'Well, this is awkward...',
+          message: 'TwitPic error: '+doc.getElementsByTagName("err").item(0).getAttribute("msg")
+        }).show();
+      }
+      else {
+        resultLabel.text = 'Upload Complete!';
+        //play sound with
+        //var yay = Titanium.Media.createSound({url:'yay.caf'});
+        //yay.play();
+      }
+
+      ind.value = 0;
+    };
+    xhr.onsendstream = function(e) {
+      ind.value = e.progress;
+    };
+    var a_username = "pablete"; //Titanium.App.Properties.getString("username"),
+    var a_password = "pablete"; //Titanium.App.Properties.getString("password"),
+    xhr.open('POST','http://dev.yumit.com/api/v0/yums.json');
+    Ti.API.info('Authorization\n'+'Basic '+Ti.Utils.base64encode(a_username+':'+a_password));
+    xhr.setRequestHeader('Authorization','Basic '+Ti.Utils.base64encode(a_username+':'+a_password));
+    xhr.setRequestHeader('Content-Type','multipart/form-data');
+    xhr.send({
+      place_id: Place.place_id,
+      dish_id: Dish.dish_id,
+      photo: win.media,
+      text: description.value,
+      tags: tags.value
+    });
+    //yay.play();
+  });
+
   win.add(scrollView);
 
 })();
